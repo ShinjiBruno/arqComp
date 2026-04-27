0e8dbd6402688f4997ac0985e24ee40a0dcc4180
ShinjiBruno <yamamotobrunoshin@gmail.com>
2026-04-27 02:09:19 -0300

    refactor: tipo de dados na ula trocado de std_logic_vector para unsigned

--- Eq12-ULA.vhd
+++ Eq12-ULA.vhd
@@ -4,20 +4,20 @@ use ieee.numeric_std.all;
 
 entity ula is
 	port (
-		in_A    : in  std_logic_vector(19 downto 0);
-		in_B    : in  std_logic_vector(19 downto 0);
-		op      : in  std_logic_vector(1 downto 0);
-		out_ULA : out std_logic_vector(19 downto 0);
+		in_A    : in  unsigned(19 downto 0);
+		in_B    : in  unsigned(19 downto 0);
+		op      : in  unsigned(1 downto 0);
+		out_ULA : out unsigned(19 downto 0);
 		zero    : out std_logic;
 		lt      : out std_logic
 	);
 end entity ula;
 
 architecture rtl of ula is
-	constant OP_ADD : std_logic_vector(1 downto 0) := "00";
-	constant OP_SUB : std_logic_vector(1 downto 0) := "01";
-	constant OP_DIV : std_logic_vector(1 downto 0) := "10";
-	constant OP_XOR : std_logic_vector(1 downto 0) := "11";
+	constant OP_ADD : unsigned(1 downto 0) := "00";
+	constant OP_SUB : unsigned(1 downto 0) := "01";
+	constant OP_DIV : unsigned(1 downto 0) := "10";
+	constant OP_XOR : unsigned(1 downto 0) := "11";
 
 	signal a_u : unsigned(19 downto 0) := (others => '0');
 	signal b_u : unsigned(19 downto 0) := (others => '0');
@@ -25,8 +25,8 @@ architecture rtl of ula is
 	signal add_res : unsigned(19 downto 0) := (others => '0');
 	signal sub_res : unsigned(19 downto 0) := (others => '0');
 	signal div_res : unsigned(19 downto 0) := (others => '0');
-	signal xor_res : std_logic_vector(19 downto 0) := (others => '0');
-	signal y_res   : std_logic_vector(19 downto 0) := (others => '0');
+	signal xor_res : unsigned(19 downto 0) := (others => '0');
+	signal y_res   : unsigned(19 downto 0) := (others => '0');
 begin
 	a_u <= unsigned(in_A);
 	b_u <= unsigned(in_B);
@@ -39,9 +39,9 @@ begin
 	xor_res <= (in_A or in_B) and (not (in_A and in_B));
 
 	with op select
-		y_res <= std_logic_vector(add_res) when OP_ADD,
-				 std_logic_vector(sub_res) when OP_SUB,
-				 std_logic_vector(div_res) when OP_DIV,
+		y_res <= unsigned(add_res) when OP_ADD,
+				 unsigned(sub_res) when OP_SUB,
+				 unsigned(div_res) when OP_DIV,
 				 xor_res                  when OP_XOR,
 				 (others => '0')          when others;
 
